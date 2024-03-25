0a3a4026f647747380e920142d3d7bd960fddc2c
Roshan Kumar Sahu <roshan@reachmobile.com>
2024-03-25 19:55:11 +0530

    Fix: migrated numeric status codes to http status codes

--- src/controllers/problems.controller.js
+++ src/controllers/problems.controller.js
@@ -1,25 +1,26 @@
+const  statusCodes = require('http-status-codes');
 function pingProblemController(req, res) {
     return res.json({message: "Ping controller is up and running"});
 }
 
 function addProblem(req, res) {
-    return res.status(501).json({message: "Not Implemented"});
+    return res.status(statusCodes.NOT_IMPLEMENTED).json({message: "Not Implemented"});
 }
 
 function getProblem(req,res) {
-    return res.status(501).json({message: "Not Implemented"});
+    return res.status(statusCodes.NOT_IMPLEMENTED).json({message: "Not Implemented"});
 }
 
 function getProblems(req, res) {
-    return res.status(501).json({message: "Not Implemented"});
+    return res.status(statusCodes.NOT_IMPLEMENTED).json({message: "Not Implemented"});
 }
 
 function deleteProblem(req, res) {
-    return res.status(501).json({message: "Not Implemented"});
+    return res.status(statusCodes.NOT_IMPLEMENTED).json({message: "Not Implemented"});
 }
 
 function updateProblem(req, res) {
-    return res.status(501).json({message: "Not Implemented"});
+    return res.status(statusCodes.NOT_IMPLEMENTED).json({message: "Not Implemented"});
 }
 //in controller, we export all functions
 module.exports = {
