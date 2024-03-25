414821d38802bfee8565634270be5f7aafeeb795
Roshan Kumar Sahu <roshan@reachmobile.com>
2024-03-25 19:13:36 +0530

    Feat: Added routing structure for versioned api problems

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const  express = require('express');
 const bodyParser = require('body-parser');
 const {PORT} = require('./config/server.config');
+const apiRouter = require("./routes");
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-app.get("/ping", (req,res) =>  {
-     return res.json({msg: 'Problem Service is Alive'})
+app.use('/api',apiRouter)
+app.get('/ping', (req,res) =>  {
+    return res.json({message: 'Problem Service is alive'});
 })
 
 app.listen(PORT,() => {
