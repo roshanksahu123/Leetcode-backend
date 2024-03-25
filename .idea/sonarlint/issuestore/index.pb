b7bb92f36b2264f3efa7d50470d4ab1eb88b9346
Roshan Kumar Sahu <roshan@reachmobile.com>
2024-03-25 18:10:11 +0530

    Feat: Added basic express server setup with dotenv and nodemon

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+K
+src/config/server.config.js,2/6/268e641304a94c72e571dd79746a19926c1b26fb
+4
+.env,3/c/3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
+<
+src/index.js,f/9/f900edc44cb929c2267a16100a32851f6d5b477d
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const  express = require('express');
+const bodyParser = require('body-parser');
+const {PORT} = require('./config/server.config');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+app.get("/ping", (req,res) =>  {
+     return res.json({msg: 'Problem Service is Alive'})
+})
+
+app.listen(PORT,() => {
+    console.log(`server started at Port: ${PORT}`);
+});
